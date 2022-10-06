183630b8a53345429b21533fc375028dbb64b192
DiegoLeonardoApazaConde <cuantodebo11@gmail.com>
2022-10-06 15:53:13 -0500

    boton info

--- metodos.js
+++ metodos.js
@@ -9,3 +9,10 @@ const cpm = () =>{
     const comp = document.getElementById("CPM")
     comp.style.display = "block";
 }
+
+const informacion = () =>{
+   const todo = document.getElementById("panelmaestro")
+   todo.style.display="none"
+   const lista = document.getElementById("info")
+   lista.style.display="block"
+}
