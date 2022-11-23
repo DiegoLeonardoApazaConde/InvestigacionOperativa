9cf0fc88a695884d751c2b4475ccc3ce60205ddb
DiegoApazaConde <cuantodebo11@gmail.com>
2022-11-23 09:41:18 -0500

    cpm terminado

--- metodos/cpm.js
+++ metodos/cpm.js
@@ -22,9 +22,8 @@ const CallData = () =>{
     for(let nm=0;nm<tamaño;nm++){
             nodosruta[nm]=contenerdor[nm][1]
     }
-    console.log(nodosruta);
-    var d = 0, prede = [], miprede = [], vectorutil = [],tiempodenodo=0
-    //Funcion para llamar el tiempo del vector de "nodos"
+    var d = 0, prede = [], miprede = [], vectorutil = [],nodostarde=[],nodoshol=[],aster=[],tiempodenodo=0,tiempodeorigen=0
+    //Funcion para llamar el tiempo del vector temprano
     function tiempoFin(letra) {
         tiempodenodo=0
             for (let ded = 0; ded < tamaño; ded++) {
@@ -33,6 +32,26 @@ const CallData = () =>{
                 }
             }
         return tiempodenodo;
+    }
+    //Funcion para llamar el tiempo del vector de "origen"
+    function tiemporeg(letru) {
+        tiempodeorigen=0
+            for (let dud = 0; dud < tamaño; dud++) {
+                if (letru == contenerdor[dud][0]) {
+                    tiempodeorigen = contenerdor[dud][1];
+                }
+            }
+        return tiempodeorigen;
+    }
+    //Funcion para llamar el tiempo del vector de "termlejana"
+    function tiempoterle(letri) {
+    tiempodenodo=0
+    for (let ded = 0; ded < tamaño; ded++) {
+        if (letri == contenerdor[ded][0]) {
+            tiempodenodo = Number(nodostarde[ded]);
+        }
+    }
+    return tiempodenodo;
     }
     //Funcion para buscar de quien soy predecesor
     function dequien(letre){
@@ -73,15 +92,65 @@ const CallData = () =>{
                         }
                     }
                     nodosruta[index] = numermay+Number(contenerdor[index][1])
-                    console.log(nodosruta);
                 }else{
                     nodosruta[index] = tiempoFin(vectorutil[0])+Number(contenerdor[index][1])
-                    console.log(nodosruta);
                 }
         }else{
             nodosruta[index] = Number(contenerdor[index][1])
-            console.log(nodosruta);
         }
     } 
-}
+    numermay = Math.max(...nodosruta) 
+    nodostarde = [...nodosruta]
+    vectorutil.length=0
+    //Bucle para asignar tiempo tardio
+    for (let indey = tamaño-1; indey>-1; indey--) {
+        if(dequien(contenerdor[indey][0]).length!=0){
+            vectorutil = dequien(contenerdor[indey][0])
+            var numeromen = vectorutil[0]
+            for(ok=0;ok<vectorutil.length;ok++){
+                if(tiempoterle(numeromen)>tiempoterle(vectorutil[ok])){
+                    numeromen = vectorutil[ok]
+                }
+            }
+            nodostarde[indey]=tiempoterle(numeromen)-tiemporeg(contenerdor[indey][0])
+        }else{
+            nodostarde[indey]=numermay-Number(contenerdor[indey][1])
 
+        }
+    }
+    //Bucle para las holguras
+    for(let hh=0;hh<tamaño;hh++){
+        nodoshol[hh]=(nodostarde[hh]+Number(contenerdor[hh][1]))-nodosruta[hh]
+        if(nodoshol[hh]==0){
+            aster[hh]="*"
+        }
+        else{
+            aster[hh]="no"
+        }
+    }
+    const tablafinres=document.getElementById("tablares")
+    const colaster = document.createElement("th")
+    const coltiempos = document.createElement("th")
+    const colruta = document.createElement("th")
+    const coltarde = document.createElement("th")
+    const colhol = document.createElement("th")
+    const filnom = document.createElement("tr")
+    colaster.append("Camino");filnom.appendChild(colaster)
+    coltiempos.append("Actividades");filnom.appendChild(coltiempos)
+    colruta.append("Ruta critica");filnom.appendChild(colruta)
+    coltarde.append("Valor de regreso");filnom.appendChild(coltarde)
+    colhol.append("Holguras");filnom.appendChild(colhol)
+    tablafinres.appendChild(filnom)
+    for(let finej=0;finej<tamaño;finej++){
+        const filanew = document.createElement("tr")
+        const colastert = document.createElement("td");colastert.append(aster[finej]);filanew.appendChild(colastert)
+        const coltiempost = document.createElement("td");coltiempost.append(contenerdor[finej][0]);filanew.appendChild(coltiempost)
+        const colrutat = document.createElement("td");colrutat.append(nodosruta[finej]);filanew.appendChild(colrutat)
+        const coltardet = document.createElement("td");coltardet.append(nodostarde[finej]);filanew.appendChild(coltardet)
+        const colholt = document.createElement("td");colholt.append(nodoshol[finej]);filanew.appendChild(colholt)
+        tablafinres.appendChild(filanew)
+    }
+    tablafinres.style.display="block"
+    alert("EL TIEMPO DE RECORRIDO ES : "+ numermay)
+    const botej = document.getElementById("leer1");botej.style.display="none" 
+}
